4579aef6a6f940fd73317b83223873eb538f5c66
David Luce <david.luce@outlook.com>
2019-08-17 22:23:44 -0600

    initial

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ('Handlebars With Cheese');
+INSERT INTO burgers (burger_name) VALUES ('MySQL Extra Bacon');
+INSERT INTO burgers (burger_name) VALUES ('Node-Beef Vegi Burger');
