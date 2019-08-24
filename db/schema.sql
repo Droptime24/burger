e97e4d33197e139a3fba2b75133c0cd8562ec909
David Luce <davidluce@davids-mbp.uconnect.utah.edu>
2019-08-24 14:32:02 -0600

    local host working

--- db/schema.sql
+++ db/schema.sql
@@ -1,11 +1,11 @@
-DROP DATABASE IF EXISTS burgers_db; 
+DROP DATABASE IF EXISTS burgers_db;
 CREATE DATABASE burgers_db;
 USE burgers_db;
 
 CREATE TABLE burgers
 (
     id INT NOT NULL AUTO_INCREMENT,
-	name(50) NOT NULL,
+	burger_names VARCHAR(50) NOT NULL,
 	devoured BOOLEAN DEFAULT false,
-	PRIMARY KEY(id)
+	PRIMARY KEY (id)
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,8 @@
-INSERT INTO burgers (burger_name) VALUES ('Handlebars With Cheese');
-INSERT INTO burgers (burger_name) VALUES ('MySQL Extra Bacon');
-INSERT INTO burgers (burger_name) VALUES ('Node-Beef Vegi Burger');
+
+INSERT INTO burgers (burger_names) VALUES ('Handlebars With Cheese');
+INSERT INTO burgers (burger_names) VALUES ('MySQL Extra Bacon');
+INSERT INTO burgers (burger_names) VALUES ('Node-Beef Burger');
+
+INSERT INTO burgers (burger_names, devoured) VALUES ('Handlebars With Cheese', true);
+INSERT INTO burgers (burger_names, devoured) VALUES ('MySQL Extra Bacon', true);
+INSERT INTO burgers (burger_names, devoured) VALUES ('Node-Beef Burger', true);
